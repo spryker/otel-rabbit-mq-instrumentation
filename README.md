ec177c172b7affa12f774ef0cf68c19ba56092f2
dimitriyTsemma <dmitry.tsemma@spryker.com>
2024-08-21 17:41:34 +0300

    FRW-8618 Init commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# otel-rabbit-mq-instrumentation
+# OtelRabbitMqInstrumentation Module
+[![Latest Stable Version](https://poser.pugx.org/spryker/otel-rabbit-mq-instrumentation/v/stable.svg)](https://packagist.org/packages/spryker/otel-rabbit-mq-instrumentation)
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%208.1-8892BF.svg)](https://php.net/)
+
+Provides an OpenTelemetry integration to RabbitMq.
+
+## Installation
+
+```
+composer require spryker/otel-rabbit-mq-instrumentation
+```
+
+## Documentation
+
+[Spryker Documentation](https://docs.spryker.com)
---
+++ src/Spryker/Service/OtelRabbitMqInstrumentation/OpenTelemetry/RabbitMqInstrumentation.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\OtelRabbitMqInstrumentation\OpenTelemetry;
+
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\API\Trace\StatusCode;
+use OpenTelemetry\Context\Context;
+use OpenTelemetry\SemConv\TraceAttributes;
+use Spryker\Client\RabbitMq\Model\RabbitMqAdapter;
+use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
+use Spryker\Shared\Opentelemetry\Request\RequestProcessor;
+use Throwable;
+use function OpenTelemetry\Instrumentation\hook;
+
+class RabbitMqInstrumentation
+{
+    /**
+     * @var string
+     */
+    protected const METHOD_NAME = 'sendMessage';
+
+    /**
+     * @return void
+     */
+    public static function register(): void
+    {
+        // phpcs:disable
+        hook(
+            class: RabbitMqAdapter::class,
+            function: static::METHOD_NAME,
+            pre: function (Client $client, array $params): void {
+                $instrumentation = new CachedInstrumentation();
+                $request = new RequestProcessor();
+
+                $span = $instrumentation::getCachedInstrumentation()
+                    ->tracer()
+                    ->spanBuilder('rabbitmq-send-message')
+//                    ->setSpanKind(SpanKind::KIND_CLIENT)
+//                    ->setAttribute(TraceAttributes::URL_FULL, $request->getRequest()->getUri())
+//                    ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getRequest()->getMethod())
+//                    ->setAttribute(TraceAttributes::URL_QUERY, $request->getRequest()->getQueryString())
+//                    ->setAttribute(TraceAttributes::URL_DOMAIN, $request->getRequest()->headers->get('host'))
+                    ->startSpan();
+                $span->activate();
+
+                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+            },
+            post: function ($instance, array $params, $response, ?Throwable $exception): void {
+                $span = Span::fromContext(Context::getCurrent());
+
+                if ($exception !== null) {
+                    $span->recordException($exception);
+                    $span->setStatus(StatusCode::STATUS_ERROR);
+                } else {
+                    $span->setAttribute('queryTime', $response->getQueryTime());
+                    $span->setStatus(StatusCode::STATUS_OK);
+                }
+
+                $span->end();
+            }
+        );
+        // phpcs:enable
+    }
+}
