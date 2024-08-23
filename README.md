d49483b0b38faf8c9118515f0ac70b779376a217
dimitriyTsemma <dmitry.tsemma@spryker.com>
2024-08-23 17:14:59 +0300

    FRW-8618 Adjusted

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![Latest Stable Version](https://poser.pugx.org/spryker/otel-rabbit-mq-instrumentation/v/stable.svg)](https://packagist.org/packages/spryker/otel-rabbit-mq-instrumentation)
 [![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%208.1-8892BF.svg)](https://php.net/)
 
-Provides an OpenTelemetry integration to RabbitMq.
+Auto-instrument RabbitMq calls in Spryker application.
 
 ## Installation
 
--- src/Spryker/Service/OtelRabbitMqInstrumentation/OpenTelemetry/RabbitMqInstrumentation.php
+++ src/Spryker/Service/OtelRabbitMqInstrumentation/OpenTelemetry/RabbitMqInstrumentation.php
@@ -7,64 +7,120 @@
 
 namespace Spryker\Service\OtelRabbitMqInstrumentation\OpenTelemetry;
 
+use Generated\Shared\Transfer\EventQueueSendMessageBodyTransfer;
+use Generated\Shared\Transfer\QueueSendMessageTransfer;
 use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\SemConv\TraceAttributes;
-use Spryker\Client\Queue\QueueClient;
 use Spryker\Client\RabbitMq\Model\RabbitMqAdapter;
 use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
 use Spryker\Shared\Opentelemetry\Request\RequestProcessor;
-use Spryker\Zed\Queue\Business\QueueFacade;
+use Spryker\Zed\Opentelemetry\Business\Generator\SpanFilter\SamplerSpanFilter;
 use Throwable;
 use function OpenTelemetry\Instrumentation\hook;
 
 class RabbitMqInstrumentation
 {
     /**
+     * @var string
+     */
+    protected const START_TIME = 'start_time';
+
+    /**
+     * @var string
+     */
+    protected const ATTRIBUTE_QUEUE_NAME = 'queue.name';
+
+    /**
+     * @var string
+     */
+    protected const HEADER_HOST = 'host';
+
+    /**
+     * @var string
+     */
+    protected const ATTRIBUTE_DURATION = 'duration';
+
+    /**
+     * @var string
+     */
+    protected const ATTRIBUTE_EVENT_LISTENER_CLASS_NAME = 'event.listenerClassName';
+
+    /**
+     * @var string
+     */
+    protected const SPAN_NAME_SEND_MESSAGES = 'rabbitmq-sendMessages';
+
+    /**
+     * @var string
+     */
+    protected const FUNCTION_SEND_MESSAGES = 'sendMessages';
+
+    /**
+     * @var string
+     */
+    protected const SPAN_NAME_SEND_MESSAGE = 'rabbitmq-sendMessage';
+
+    /**
+     * @var string
+     */
+    protected const FUNCTION_SEND_MESSAGE = 'sendMessage';
+
+    /**
      * @return void
      */
     public static function register(): void
     {
-        // phpcs:disable
+        $functions = [
+            static::FUNCTION_SEND_MESSAGE => static::SPAN_NAME_SEND_MESSAGE,
+            static::FUNCTION_SEND_MESSAGES => static::SPAN_NAME_SEND_MESSAGES,
+        ];
+
+        foreach ($functions as $function => $spanName) {
+            static::registerHook($function, $spanName);
+        }
+    }
+
+    /**
+     * @param string $functionName
+     * @param string $spanName
+     *
+     * @return void
+     */
+    protected static function registerHook(string $functionName, string $spanName): void
+    {
+        $instrumentation = CachedInstrumentation::getCachedInstrumentation();
+        $request = (new RequestProcessor())->getRequest();
+
         hook(
             class: RabbitMqAdapter::class,
-            function: 'sendMessage',
-            pre: function (RabbitMqAdapter $rabbitMqAdapter, array $params): void {
-                $instrumentation = new CachedInstrumentation();
-                $request = new RequestProcessor();
+            function: $functionName,
+            pre: function (RabbitMqAdapter $rabbitMqAdapter, array $params) use ($instrumentation, $spanName, $request): void {
                 $startTime = microtime(true);
 
-                $span = $instrumentation::getCachedInstrumentation()
-                    ->tracer()
-                    ->spanBuilder('rabbitmq-sendMessage')
-                    ->setAttribute('start_time', $startTime)
+                $span = $instrumentation->tracer()
+                    ->spanBuilder($spanName)
                     ->setSpanKind(SpanKind::KIND_CLIENT)
-                    ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getRequest()->getMethod())
-                    ->setAttribute('queue.name', $params[0]);
-                if(
-                    array_key_exists(1, $params)
-                    && array_key_exists(0, $params[1])
-                    && is_string($params[1][0])
-                ) {
+                    ->setAttribute(static::START_TIME, $startTime)
+                    ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getMethod())
+                    ->setAttribute(static::ATTRIBUTE_QUEUE_NAME, $params[0]);
+
+                if (static::isValidMessage($params)) {
                     $array = json_decode($params[1][0]->getBody(), true);
-                    if (array_key_exists('eventName', $array)) {
-                        $span->setAttribute(TraceAttributes::EVENT_NAME, $array['eventName']);
+                    if (array_key_exists(EventQueueSendMessageBodyTransfer::EVENT_NAME, $array)) {
+                        $span->setAttribute(TraceAttributes::EVENT_NAME, $array[EventQueueSendMessageBodyTransfer::EVENT_NAME]);
                     }
-                    if (array_key_exists('listenerClassName', $array)) {
-                        $span->setAttribute('event.listenerClassName', $array['listenerClassName']);
-                    }
-                    if (array_key_exists('transferClassName', $array)) {
-                        $span->setAttribute('event.transferClassName', $array['eventName']);
+                    if (array_key_exists(EventQueueSendMessageBodyTransfer::LISTENER_CLASS_NAME, $array)) {
+                        $span->setAttribute(static::ATTRIBUTE_EVENT_LISTENER_CLASS_NAME, $array[EventQueueSendMessageBodyTransfer::LISTENER_CLASS_NAME]);
                     }
                 }
 
-                $span->setAttribute(TraceAttributes::URL_DOMAIN, $request->getRequest()->headers->get('host'))
+                $span
+                    ->setAttribute(TraceAttributes::URL_DOMAIN, $request->headers->get(static::HEADER_HOST))
                     ->startSpan()
                     ->activate();
-
-                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
             },
             post: function ($instance, array $params, $response, ?Throwable $exception): void {
                 $span = Span::fromContext(Context::getCurrent());
@@ -73,71 +129,30 @@ class RabbitMqInstrumentation
                     $span->recordException($exception);
                     $span->setStatus(StatusCode::STATUS_ERROR);
                 } else {
+
                     $span->setStatus(StatusCode::STATUS_OK);
                 }
+
                 $endTime = microtime(true);
-                $duration = $endTime - $span->getAttribute('start_time');
-                $span->setAttribute('start_time', null);
-                $span->setAttribute('duration', $duration);
+                $duration = $endTime - $span->getAttribute(static::START_TIME);
+                $span->setAttribute(static::START_TIME, null);
+                $span->setAttribute(static::ATTRIBUTE_DURATION, $duration);
 
                 $span->end();
             }
         );
-        hook(
-            class: RabbitMqAdapter::class,
-            function: 'sendMessages',
-            pre: function (RabbitMqAdapter $rabbitMqAdapter, array $params): void {
-                $instrumentation = new CachedInstrumentation();
-                $request = new RequestProcessor();
-                $startTime = microtime(true);
+    }
 
-                $span = $instrumentation::getCachedInstrumentation()
-                    ->tracer()
-                    ->spanBuilder('rabbitmq-sendMessages')
-                    ->setAttribute('start_time', $startTime)
-                    ->setSpanKind(SpanKind::KIND_CLIENT)
-                    ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getRequest()->getMethod())
-                    ->setAttribute('queue.name', $params[0]);
-                if(
-                    array_key_exists(1, $params)
-                    && array_key_exists(0, $params[1])
-                    && is_string($params[1][0])
-                ) {
-                    $array = json_decode($params[1][0]->getBody(), true);
-                        if (array_key_exists('eventName', $array)) {
-                            $span->setAttribute(TraceAttributes::EVENT_NAME, $array['eventName']);
-                        }
-                        if (array_key_exists('listenerClassName', $array)) {
-                            $span->setAttribute('event.listenerClassName', $array['listenerClassName']);
-                        }
-                        if (array_key_exists('transferClassName', $array)) {
-                            $span->setAttribute('event.transferClassName', $array['eventName']);
-                        }
-                }
-
-                    $span->setAttribute(TraceAttributes::URL_DOMAIN, $request->getRequest()->headers->get('host'))
-                    ->startSpan()
-                    ->activate();
-
-                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
-            },
-            post: function ($instance, array $params, $response, ?Throwable $exception): void {
-                $span = Span::fromContext(Context::getCurrent());
-
-                if ($exception !== null) {
-                    $span->recordException($exception);
-                    $span->setStatus(StatusCode::STATUS_ERROR);
-                } else {
-                    $span->setStatus(StatusCode::STATUS_OK);
-                }
-                $endTime = microtime(true);
-                $duration = $endTime - $span->getAttribute('start_time');
-                $span->setAttribute('start_time', null);
-                $span->setAttribute('duration', $duration);
-
-                $span->end();
-            }
-        );
-        // phpcs:enable
+    /**
+     * @param array<int, mixed> $params
+     *
+     * @return bool
+     */
+    protected static function isValidMessage(array $params): bool
+    {
+        return array_key_exists(1, $params)
+            && array_key_exists(0, $params[1])
+            && $params[1][0] instanceof QueueSendMessageTransfer
+            && is_string($params[1][0]->getBody());
     }
 }
